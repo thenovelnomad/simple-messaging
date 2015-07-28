7b2ae0e85590108e071849c75879333716d95bc2
Jonathan Limbird <jonathan.limbird@gmail.com>
2015-07-28 11:04:38 -0500

    Break up logic into parsing and evaluating in evaluator

--- test/evaluator.js
+++ test/evaluator.js
@@ -2,37 +2,71 @@ var assert = require( 'assert' ),
     evaluator = require( '../lib/evaluator.js' );
 
 describe( 'Evaluator', function() {
-    describe('Given a request url of "http://localhost:3000/"', function() {
-        it( 'should return ""', function() {
-            assert.equal( evaluator('http://localhost:3000/'), null );
+    describe( 'parseRequest', function() {
+        describe('Given a request url of "http://localhost:3000/"', function() {
+            it( 'should return ""', function() {
+                assert.equal( evaluator.parseRequest('http://localhost:3000/'), null );
+            });
+        });
+
+        describe('Given a request url of "http://localhost:3000/" with query string of form "A+B=" where A and B are integers', function() {
+            it( 'should return the query string "A+B="', function() {
+                assert.equal( typeof evaluator.parseRequest( 'http://localhost:3000/?12+5=' ), 'string' );
+
+                assert.equal( evaluator.parseRequest( 'http://localhost:3000/?12+5=' ), '12+5=' );
+            });
+        });
+
+        describe( 'Given a request url of "http://localhost:3000/books"', function() {
+            it( 'should return an error', function() {
+                assert.throws( function() { 
+                    evaluator.parseRequest( 'http://localhost:3000/books' );
+                }, Error );
+            });
         });
     });
 
-    describe('Given a request url of "http://localhost:3000/" with query string of form "A+B=" where A and B are integers', function() {
-        it( 'should return the sum of A and B as a string', function() {
-            assert.equal( typeof evaluator( 'http://localhost:3000/?12+5=' ), 'string' );
-
-            assert.equal( evaluator( 'http://localhost:3000/?12+5=' ), '17' );
+    describe( 'getSum', function() {
+        describe( 'given a query string of form "A+B=" where A and B are integers', function() {
+            it( 'should return the sum of A and B as a string', function() {
+                assert.equal( typeof evaluator.getSum( '12+3=' ), 'string' );
+                assert.equal( evaluator.getSum( '12+3=' ), '15' );
+            });
         });
-    });
 
-    describe( 'Given a request url where the query string is not of the form "A+B=" where A and B are integers', function() {
-        it( 'should throw an error', function() {
-            assert.throws( function() {
-                evaluator( 'http://localhost:3000/?B+C=' );
-            }, Error );
+        describe( 'given a query string of form "A+B=" where A and B are integers', function() {
+            it( 'should return the sum of A and B as a string', function() {
+                assert.equal( typeof evaluator.getSum( '12+3=' ), 'string' );
+                assert.equal( evaluator.getSum( '12+3=' ), '15' );
+            });
+        });
 
-            assert.throws( function() {
-                evaluator( 'http://localhost:3000/?12.2+3=' );
-            }, Error );
+        describe( 'Given a query string is not of the form "A+B=" where A and B are integers', function() {
+            it( 'should throw an error', function() {
+                assert.throws( function() {
+                    evaluator.parseRequest( 'B+C=' );
+                }, Error );
 
-            assert.throws( function() {
-                evaluator( 'http://localhost:3000/?12-3=' );
-            }, Error );
+                assert.throws( function() {
+                    evaluator.parseRequest( '12.2+3=' );
+                }, Error );
 
-            assert.throws( function() {
-                evaluator( 'http://localhost:3000/?12+3/' );
-            }, Error );
+                assert.throws( function() {
+                    evaluator.parseRequest( '12-3=' );
+                }, Error );
+
+                assert.throws( function() {
+                    evaluator.parseRequest( 2 );
+                }, Error );
+
+                assert.throws( function() {
+                    evaluator.parseRequest( [] );
+                }, Error );
+
+                assert.throws( function() {
+                    evaluator.parseRequest( {} );
+                }, Error );
+            });
         });
     });
 });
